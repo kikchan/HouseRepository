09728a9ba8af8e3601fdee4e16e0d00f977adae9
Kikchan <kiril_gaydarov@abv.bg>
2026-06-19 13:33:15 +0200

    Test

--- src/pages/DashboardPage.jsx
+++ src/pages/DashboardPage.jsx
@@ -117,7 +117,7 @@ export default function DashboardPage({ user, onLogout }) {
                   onClick={() => navigate('/houses/new')}
                   className={`${buttonClass} coast-button-primary`}
                 >
-                  Add house
+                  Add house bre
                 </button>
               </div>
             </div>
